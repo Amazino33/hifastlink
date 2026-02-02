fa4f9091a1dbc8851d8155b0293022b1cc3038e0
Osmund Peter <amazino33@gmail.com>
2026-02-02 15:36:11 +0100

    paystack resolve

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            'payment/webhook', // OR 'paystack/webhook' - Check your route name
+        ]);
+    })
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
