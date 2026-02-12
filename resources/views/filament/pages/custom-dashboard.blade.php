f0ad0241820ee197ff8ab71f03b14371dd0c1c3e
Osmund Peter <amazino33@gmail.com>
2026-02-12 11:18:39 +0100

    Move no-scrollbar styles to router-filter component for better organization

--- resources/views/filament/pages/custom-dashboard.blade.php
+++ resources/views/filament/pages/custom-dashboard.blade.php
@@ -3,9 +3,4 @@
         @livewire('router-filter')
         @livewire('admin-stats')
     </div>
-
-    <style>
-        .no-scrollbar::-webkit-scrollbar { display: none; }
-        .no-scrollbar { -ms-overflow-style: none; scrollbar-width: none; }
-    </style>
 </x-filament-panels::page>
--- resources/views/livewire/router-filter.blade.php
+++ resources/views/livewire/router-filter.blade.php
@@ -16,6 +16,11 @@
     </div>
 </div>
 
-
+@push('styles')
+<style>
+.no-scrollbar::-webkit-scrollbar { display: none; }
+.no-scrollbar { -ms-overflow-style: none; scrollbar-width: none; }
+</style>
+@endpush
 
 </div>
